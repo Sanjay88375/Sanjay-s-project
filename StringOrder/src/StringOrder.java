9f8e2ed3b4cc773ac9716a3d1482da224083232d
pappupc <pappu@gmail.com>
2022-08-25 15:17:22 +0530

    Commit

--- StringOrder/src/StringOrder.java
+++ StringOrder/src/StringOrder.java
@@ -1,9 +1,9 @@
 import java.util.Arrays;
 public class StringOrder {
    public static void main(String args[]) {
-      String str = "This is a easiest example";
-	  char charArray[] = str.toCharArray();
-      Arrays.sort(charArray);
+      String str = "This is a easiest example"; // taking String
+	  char charArray[] = str.toCharArray(); // Defining char to read the characters
+      Arrays.sort(charArray); // for sorting
       System.out.println(new String(charArray));
    }
 }
